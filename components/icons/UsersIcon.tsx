ea73ef540c83e285081463a1433cdad19163bc53
Prakashtailorr <shawamimohit@gmail.com>
2025-07-23 17:47:48 +0530

    Create UsersIcon.tsx

---
+++ components/icons/UsersIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const UsersIcon: React.FC = () => {
+  return (
+    <svg
+      className="w-6 h-6 text-blue-500"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M17 20h5v-2a4 4 0 00-3-3.87M9 20H4v-2a4 4 0 013-3.87M16 7a4 4 0 11-8 0 4 4 0 018 0zm6 13v-2a4 4 0 00-3-3.87M6 10a4 4 0 00-3 3.87v2"
+      />
+    </svg>
+  );
+};
+
+export default UsersIcon;
