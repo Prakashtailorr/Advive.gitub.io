ed1c20c21a0c6278815002848a13b699831aa5cf
Prakashtailorr <shawamimohit@gmail.com>
2025-07-23 17:40:46 +0530

    Create GiftIcon.tsx

---
+++ components/GiftIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const GiftIcon: React.FC = () => {
+  return (
+    <svg
+      className="w-6 h-6 text-pink-500"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M20 12v7a2 2 0 01-2 2H6a2 2 0 01-2-2v-7m16 0H4m16 0V7a2 2 0 00-2-2h-3.5m-7 0H6a2 2 0 00-2 2v5m4.5-7A2.5 2.5 0 1112 7v0a2.5 2.5 0 11-2.5-2.5z"
+      ></path>
+    </svg>
+  );
+};
+
+export default GiftIcon;
