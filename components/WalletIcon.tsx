176f97dfbab9bfe8eb900b3c0000860f3dfe1c7e
Prakashtailorr <shawamimohit@gmail.com>
2025-07-23 17:33:00 +0530

    Create WalletIcon.tsx

---
+++ components/WalletIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WalletIcon: React.FC = () => {
+  return (
+    <svg
+      className="w-6 h-6 text-yellow-500"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
